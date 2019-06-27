80609f9b511e5968650747f6a0a828d578794b24
Shawnkeith <Shawnkeith1997@gmail.com>
2019-06-27 10:17:08 -0500

    Merge sort problem started

--- JavaScript Toy problem.js
+++ JavaScript Toy problem.js
@@ -557,46 +557,97 @@
 // console.log(`Test2: expected [ { "value": 5, "order": 1 }, { "value": 5, "order": 2 }, { "value": 10 } ] and got ${JSON.stringify(test2)}`);
 
 
-var quickSort = function(arr){
-  //choose starting element
-  let startingElement = arr.length-1
-  //create an array for holding values less that and greater than starting element
-  let lessThan = [];
-  let greaterThan = [];
-  let results = [];
-  //loop through array
-  if(arr.length === 0 || arr.length === 1){
-    return arr;
-  };
-  for(let i =0; i < arr.length; i++){
-    let currentElement = arr[i]
-    if(currentElement < startingElement){
-      //if index is less that starting element push into less than array
-      lessThan.push(currentElement)
-      // console.log(lessThan)
-    }else if(currentElement > startingElement){
-      //if index is greater than push into greater than array
-      greaterThan.push(currentElement)
-      // console.log(greaterThan)
-    }
+// var quickSort = function(arr){
+//   //choose starting element
+//   let startingElement = arr.length-1
+//   //create an array for holding values less that and greater than starting element
+//   let lessThan = [];
+//   let greaterThan = [];
+//   //loop through array
+//   if(arr.length === 0 || arr.length === 1){
+//     return arr;
+//   };
+//   for(let i =0; i < arr.length; i++){
+//     let currentElement = arr[i]
+//     if(currentElement < startingElement){
+//       //if index is less that starting element push into less than array
+//       lessThan.push(currentElement)
+//       // console.log(lessThan)
+//     }else if(currentElement > startingElement){
+//       //if index is greater than push into greater than array
+//       greaterThan.push(currentElement)
+//       // console.log(greaterThan)
+//     }
+//   }
+//   //recursively call function on each new array
+//   //concat the two arrays and the starting element
+//   return quickSort(lessThan).concat(startingElement, greaterThan)
+// }
+
+
+// const test1 = quickSort([8,7,3,6,9,2,4,5,1]);
+// console.log(`Should sort a short array of integers: expected [1,2,3,4,5,6,7,8,9] and got ${JSON.stringify(test1)}`);
+
+// const test2 = quickSort([8,7,3,3,9,2,4,5,1]);
+// console.log(`Should handle duplicates: expected [1,2,3,3,4,5,7,8,9] and got ${JSON.stringify(test2)}`);
+
+// const test3 = quickSort([9,8,7,6,5,4,3,2,1]);
+// console.log(`Should handle reversely sorted array: expected [1,2,3,4,5,6,7,8,9] and got ${JSON.stringify(test3)}`);
+
+// const test4 = quickSort([]);
+// console.log(`Should handle empty array: expected [] and got ${JSON.stringify(test4)}`);
+
+// const test5 = quickSort([1]);
+// console.log(`Should handle array with single element: expected [1] and got ${JSON.stringify(test5)}`);
+
+
+
+//mergesort
+//will split the array elements into individual arrays
+//new individual arrays will be merged with the array beside it and compared/sorted
+//repeate the previous step until each single array has been merged and sorted
+
+var mergeSort = function(arr){
+  var holdingArr = [];
+  //loop through the array and make every index an individual array
+  for(let i = 0; i < arr.length; i ++){
+    console.log(arr[i])
+    // holdingArr = arr.splice(arr[i])
   }
-  //recursively call function on each new array
-  //concat the two arrays and the starting element
-  return quickSort(lessThan).concat(startingElement, greaterThan)
+  console.log(holdingArr);
+  //for each index merge then compare to the index +1
+  //continue this for the length of the array 
 }
 
 
-const test1 = quickSort([8,7,3,6,9,2,4,5,1]);
+const test1 = mergeSort([8,7,3,6,9,2,4,5,1]);
 console.log(`Should sort a short array of integers: expected [1,2,3,4,5,6,7,8,9] and got ${JSON.stringify(test1)}`);
 
-const test2 = quickSort([8,7,3,3,9,2,4,5,1]);
+const test2 = mergeSort([8,7,3,3,9,2,4,5,1]);
 console.log(`Should handle duplicates: expected [1,2,3,3,4,5,7,8,9] and got ${JSON.stringify(test2)}`);
 
-const test3 = quickSort([9,8,7,6,5,4,3,2,1]);
+const test3 = mergeSort([9,8,7,6,5,4,3,2,1]);
 console.log(`Should handle reversely sorted array: expected [1,2,3,4,5,6,7,8,9] and got ${JSON.stringify(test3)}`);
 
-const test4 = quickSort([]);
+const test4 = mergeSort([]);
 console.log(`Should handle empty array: expected [] and got ${JSON.stringify(test4)}`);
 
-const test5 = quickSort([1]);
+const test5 = mergeSort([1]);
 console.log(`Should handle array with single element: expected [1] and got ${JSON.stringify(test5)}`);
+
+function createLargeArray() {
+  var input = [];
+  var sorted;
+  var n = 100000;
+  for (var i = 0; i < n; i++) {
+    var number = Math.floor(Math.random() * n);
+    input.push(number);
+  }
+
+  return input;
+}
+
+const arr = createLargeArray();
+const test7 = arr.slice().sort((a, b) => a - b);
+const test6 = mergeSort(arr);
+console.log(`Should handle large array: ${JSON.stringify(test6) === JSON.stringify(test7)}`);
